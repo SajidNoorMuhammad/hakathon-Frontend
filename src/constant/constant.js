e6995821cb997f8a1a04fa1520940bae8248f8f3
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2025-01-27 14:36:35 +0500

    update.

---
+++ src/constant/constant.js
@@ -0,0 +1,7 @@
+let devURL = "http://localhost:5000/";
+export const BASE_URL = devURL;
+
+export const AppRoutes = {
+    getLoans: BASE_URL + 'admin/loans',
+    register: BASE_URL + 'adminusers/register'
+}
